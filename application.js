b294135df08ce5b340a184530442b9439f378b2b
Kevin Anderson <kcanderson1969@gmail.com>
2015-06-29 22:47:45 -0500

    Fourth Commit

--- application.js
+++ application.js
@@ -1,33 +1,44 @@
-//Define Globals
-  var toDoList = [];
+// Define Global
+var toDoList = [];
   
-  function changeDisplay(){
+function changeDisplay(){
    var list = document.getElementById("todolist");
    // Only place we actually change the screen
    list.innerHTML = "";
    for(var i=0; i<toDoList.length; i++) {
-   list.innerHTML += '<div class="item">' + toDoList[i] + '</div>';
+   list.innerHTML += '<div class="item">' + toDoList [i] +
+   ' (<a href="#" onclick="javascript:removeToDo(' + i + ')">x</a>)' +
+   '</div>';
    }
+}
+ 
+function removeToDo(itemToRemove){
+  var newList = [];
+  for (var i=0;i<toDoList.length;i++) {
+    if (i !== itemToRemove) {
+      newList.push(toDoList[i]);
+    }
   }
-  
+  toDoList=newList;
+  changeDisplay();
+} 
+ 
 function addToDo(){
-
 
   var tmpItem;
   var newToDo = document.getElementById("todonew");
-  
-
   tmpItem = newToDo.value;
-
-    
+  
+  if(tmpItem == ""){
+    alert("You didn't put anything in to add");
+    return;
+  }
   toDoList.push(tmpItem);
   // Call our display function
   changeDisplay();
   
   newToDo.value = "";
   newToDo.select();
-  
-  
 }
 
 
