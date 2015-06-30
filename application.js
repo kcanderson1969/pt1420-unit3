e4cf154d5b6d2dd6fea70c9c8c3b13367f706d81
Kevin Anderson <kcanderson1969@gmail.com>
2015-06-29 21:15:54 -0500

    Second Commit

--- application.js
+++ application.js
@@ -1,1 +1,36 @@
-// alert("hello World")
+//alert("hello World")
+  var toDoList = [];
+  
+function addToDo(){
+
+  var list;
+  var tmpItem;
+  
+    list = document.getElementById("todolist");
+    tmpItem = document.getElementById("todonew").value;
+    list.innerHTML = "";
+    
+    toDoList.push(tmpItem)
+   for(var i=0; i<toDoList.length; i++) {
+     list.innerHTML += '<div class="item">' + toDoList[i] + '</div>';
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
